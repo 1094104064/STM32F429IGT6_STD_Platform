b9c8971728f0ae046edd91543b9359e48c5d769d
GarMing <1094104064@qq.com>
2025-09-04 23:26:08 +0800

    refactor(bsp): optimize the head file of driver_lcd

--- USER/04_BSP/driver/bsp_driver_lcd.h
+++ USER/04_BSP/driver/bsp_driver_lcd.h
@@ -29,52 +29,51 @@ extern "C" {
  **********************/
 
 struct lcd_layer {
-    uint8_t pixel_size;
-    uint16_t rotated;
-    uint32_t start_address;
+    uint8_t     pixel_size;
+    uint16_t    rotated;
+    uint32_t    start_address;
 };
 
 
 
 struct lcd_panel {
 
-    const char * name;
+    const char *    name;
 
-    uint16_t active_width;
-    uint16_t active_height;
+    uint16_t        active_width;
+    uint16_t        active_height;
 
-    uint32_t hsync_width;
-    uint32_t hback_porch;
-    uint32_t hfront_porch;
-    uint32_t vback_porch;
-    uint32_t vsync_width;
-    uint32_t vfront_porch;
-
+    uint32_t        hsync_width;
+    uint32_t        hback_porch;
+    uint32_t        hfront_porch;
+    uint32_t        vback_porch;
+    uint32_t        vsync_width;
+    uint32_t        vfront_porch;
 };
 
 
 struct lcd_oper {
-    void (* pf_layer_config)(uint8_t * pixel_size, uint16_t * rotated, uint32_t * start_address);
-    void (* pf_backlight_on)(void);
-    void (* pf_backlight_off)(void);
-    void (* pf_put_pixel)(uint16_t x, uint16_t y, uint32_t color);
-    void (* pf_fast_fill_rect)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
-    void (* pf_fast_fill_screen)(uint32_t color);
-    void (* pf_copy_buffer)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t * data);
+    void (* pf_layer_config)        (uint8_t * pixel_size, uint16_t * rotated, uint32_t * start_address);
+    void (* pf_backlight_on)        (void);
+    void (* pf_backlight_off)       (void);
+    void (* pf_put_pixel)           (uint16_t x, uint16_t y, uint32_t color);
+    void (* pf_fast_fill_rect)      (uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+    void (* pf_fast_fill_screen)    (uint32_t color);
+    void (* pf_copy_buffer)         (uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t * data);
 };
 
 struct lcd_driver {
-    struct lcd_oper * oper;
-    const struct lcd_panel * panel;
-    struct lcd_layer * layer;
+    struct lcd_oper *           oper;
+    const struct lcd_panel *    panel;
+    struct lcd_layer *          layer;
 
-    bool (* pf_init)(struct lcd_driver * self);
-    void (* pf_backlight_on)(struct lcd_driver * self);
-    void (* pf_backlight_off)(struct lcd_driver * self);
-    void (* pf_put_pixel)(struct lcd_driver * self, uint16_t x, uint16_t y, uint32_t color);
-    void (* pf_fill_rect)(struct lcd_driver * self, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
-    void (* pf_fill_screen)(struct lcd_driver * self, uint32_t color);
-    void (* pf_copy_buffer)(struct lcd_driver * self, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t * data);
+    bool (* pf_init)            (struct lcd_driver * self);
+    void (* pf_backlight_on)    (struct lcd_driver * self);
+    void (* pf_backlight_off)   (struct lcd_driver * self);
+    void (* pf_put_pixel)       (struct lcd_driver * self, uint16_t x, uint16_t y, uint32_t color);
+    void (* pf_fill_rect)       (struct lcd_driver * self, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+    void (* pf_fill_screen)     (struct lcd_driver * self, uint32_t color);
+    void (* pf_copy_buffer)     (struct lcd_driver * self, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t * data);
 };
 
 /**********************
