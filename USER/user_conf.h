dfbf88a7d9e1e90becd9864cf53aa87ea353de5f
GarMing <1094104064@qq.com>
2025-09-05 17:57:05 +0800

    release v1.0.3

--- USER/user_conf.h
+++ USER/user_conf.h
@@ -27,7 +27,7 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
-#define PLATFORM_VERSION "1.0.2"
+#define PLATFORM_VERSION "1.0.3"
 /**********************
  *      TYPEDEFS
  **********************/
